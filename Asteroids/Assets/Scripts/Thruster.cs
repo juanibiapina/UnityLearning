f6ebc20c6498c117fade7269bcfb1c0ee7fb2e76
Juan Ibiapina <juanibiapina@gmail.com>
2021-03-07 19:35:47 +0100

    Initial Asteroids

---
+++ Asteroids/Assets/Scripts/Thruster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thruster : MonoBehaviour
+{
+    public KeyCode trigger = KeyCode.Space;
+    public float power = 5.0f;
+
+    Rigidbody body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool active = Input.GetKey(trigger);
+        
+        if (active) {
+            body.AddRelativeForce(Vector3.up * power);
+        }
+    }
+}
