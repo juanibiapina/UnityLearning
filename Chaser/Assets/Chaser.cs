378ffd498fc58e923b07c123afae1092dc7e772c
Juan Ibiapina <juanibiapina@gmail.com>
2021-03-07 14:29:58 +0100

    Chaser

---
+++ Chaser/Assets/Chaser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour
+{
+    public float speed = 5;
+    public float attackRange = 1.5f;
+    public Transform target;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 displacement = target.position - transform.position;
+        Vector3 dir = displacement.normalized;
+        float distance = displacement.magnitude;
+
+        if (distance > attackRange) {
+Vector3 offset = dir * (speed * Time.deltaTime);
+
+        transform.Translate(offset);
+        }
+    }
+}
---
+++ Chaser/Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 7;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 dir = input.normalized;
+        Vector3 offset = dir * (speed * Time.deltaTime);
+        transform.Translate(offset);
+    }
+}
