e9073782247ab5e778ddd684000ebdfe07d2531e
Juan Ibiapina <juanibiapina@gmail.com>
2021-03-08 18:53:53 +0100

    go

--- Asteroids2D/Assets/Player.cs
+++ Asteroids2D/Assets/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipController : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public float rotationSpeed;
     public float thrust;
