9e3f666e723be0ac6489fadd3710bc234940c97c
Juan Ibiapina <juanibiapina@gmail.com>
2021-03-08 14:44:25 +0100

    Asteroids2D

---
+++ Asteroids2D/Assets/ShipController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float thrust;
+    Rigidbody2D body;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float rotationDirection = Input.GetAxisRaw("Horizontal");
+
+        body.MoveRotation(body.rotation - (rotationSpeed * rotationDirection * Time.deltaTime));
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            body.AddRelativeForce(Vector2.up * thrust);
+        }
+    }
+}
