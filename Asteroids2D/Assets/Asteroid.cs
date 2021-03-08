992f5d07f23e95f415252b653bd534901ff852fc
Juan Ibiapina <juanibiapina@gmail.com>
2021-03-08 16:21:21 +0100

    Screen wrap asteroid

--- Asteroids2D/Assets/Asteroid.cs
+++ Asteroids2D/Assets/Asteroid.cs
@@ -1,16 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    Vector2 screenHalfSize;
+    Vector2 halfSize;
+
     void Start()
     {
-        
+        CalculateScreenHalfSize();
+        CalculateSize();
     }
 
     void Update()
     {
-        
+        WrapAround();
+    }
+    
+    private void CalculateSize()
+    {
+        halfSize = new Vector2(transform.localScale.x / 2, transform.localScale.y / 2);
+    }
+
+    private void WrapAround()
+    {
+        Vector2 pos = transform.position;
+
+        if (pos.x < -screenHalfSize.x - halfSize.x)
+        {
+            pos.x = screenHalfSize.x + halfSize.x;
+        }
+        else if (pos.x > screenHalfSize.x + halfSize.x)
+        {
+            pos.x = -screenHalfSize.x - halfSize.x;
+        }
+
+        if (pos.y < -screenHalfSize.y - halfSize.y)
+        {
+            pos.y = screenHalfSize.y + halfSize.y;
+        }
+        else if (pos.y > screenHalfSize.y + halfSize.y)
+        {
+            pos.y = -screenHalfSize.y - halfSize.y;
+        }
+
+        transform.position = pos;
+    }
+
+    void CalculateScreenHalfSize()
+    {
+        screenHalfSize = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
     }
 }
