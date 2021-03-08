8f02c76f7a15a1f26c5e3a710608c0f16b4f7c66
Juan Ibiapina <juanibiapina@gmail.com>
2021-03-08 17:21:43 +0100

    stuff

---
+++ Asteroids2D/Assets/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 1.0f);
+        GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * 400);       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Asteroids2D/Assets/ShipController.cs
+++ Asteroids2D/Assets/ShipController.cs
@@ -1,15 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    public GameObject bulletPrefab;
     public float rotationSpeed;
     public float thrust;
     Rigidbody2D body;
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShootBullet();
+        }
     }
 
     void FixedUpdate()
@@ -22,11 +32,18 @@ public class ShipController : MonoBehaviour
         {
             body.AddRelativeForce(Vector2.up * thrust);
         }
-
-        
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
-        Destroy(this.gameObject);
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Asteroid")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void ShootBullet()
+    {
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
     }
 }
