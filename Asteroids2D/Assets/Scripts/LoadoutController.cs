dd286c15628bdfe4627622e6b69efbb96bc4132e
Juan Ibiapina <juanibiapina@gmail.com>
2021-03-17 23:45:13 +0100

    add loadout scene, fix bugs

---
+++ Asteroids2D/Assets/Scripts/LoadoutController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadoutController : MonoBehaviour
+{
+    public void Go()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
