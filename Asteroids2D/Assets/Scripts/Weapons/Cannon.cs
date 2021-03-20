bbc166d6f18dff3b357e527bf683dba4381a9d90
Juan Ibiapina <juanibiapina@gmail.com>
2021-03-20 14:36:35 +0100

    split cannon weapon

--- Asteroids2D/Assets/Scripts/Weapons/Cannon.cs
+++ Asteroids2D/Assets/Scripts/Weapons/Cannon.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletShooter : MonoBehaviour
+public class Cannon : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    public float bulletCooldown;
+    public float bulletCooldown = 0.75f;
 
     float lastShotTime;
     void Start()
