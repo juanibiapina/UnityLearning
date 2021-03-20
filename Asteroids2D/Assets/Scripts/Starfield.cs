4f8cb67cd6a72f3e44a3c31ff833b5a2401996d2
Juan Ibiapina <juanibiapina@gmail.com>
2021-03-20 13:43:05 +0100

    fix starfield and other things

---
+++ Asteroids2D/Assets/Scripts/Starfield.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starfield : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParticleSystem p = GetComponent<ParticleSystem>();
+        ParticleSystem.ShapeModule shape = p.shape;
+        shape.radius = Camera.main.aspect * Camera.main.orthographicSize;
+        p.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
