bdcd1bc995d18eefb49f06f235f62872b562c4d2
Juan Ibiapina <juanibiapina@gmail.com>
2021-03-21 15:57:06 +0100

    pusher shield

---
+++ Asteroids2D/Assets/Scripts/WeaponItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponItem : MonoBehaviour {
+    public GameObject weaponPrefab;
+
+    void OnTriggerEnter2D(Collider2D c) {
+        if (c.gameObject.tag == "Player") {
+            // self destruct
+            Destroy(gameObject);
+
+            // set weapon from prefab
+            Player player = c.gameObject.GetComponent<Player>();
+            player.SetWeapon(weaponPrefab);
+        }
+    }
+}
---
+++ Asteroids2D/Assets/Scripts/Weapons/PusherShield.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PusherShield : MonoBehaviour {
+    public GameObject prefab;
+    public float cooldown = 3;
+
+    float lastShotTime;
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            Shoot();
+        }
+    }
+
+    private void Shoot() {
+        if (Time.time - lastShotTime > cooldown) {
+            Instantiate(prefab, transform);
+            lastShotTime = Time.time;
+        }
+    }
+}
